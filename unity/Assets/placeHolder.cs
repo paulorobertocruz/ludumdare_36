a9864a1c261262f708e489a25af317663b9d6347
Guz1992 <gustavogpi25@gmail.com>
2016-08-27 00:54:43 -0300

    criada cena inicial e script temporario
    tudo ok

---
+++ unity/Assets/placeHolder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class placeHolder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
