d1c5df7cebed434236456ba69bdf48c89c968c14
paulorobertocruz <paulo.cruz9@gmail.com>
2016-08-27 18:31:37 -0300

    adicionados funções que mudam o estado de conforto do plaeyer

--- unity/Assets/scripts/CharacterController.cs
+++ unity/Assets/scripts/CharacterController.cs
@@ -8,15 +8,19 @@ public class CharacterController : MonoBehaviour {
 
 	int sanidade;
 	public int max_sanidade = 0;
+	bool safe_sanidade = false;
 
 	int calor_corporal;
 	public int max_calor_corporal = 0;
+	bool safe_calor_corporal = false;
+
+
 
 	// Use this for initialization
 	void Start () {
 		hp = max_hp;
 
-		//
+		//começa com a metade da sanidade maxima
 		sanidade = max_sanidade / 2;
 
 		//começa com 10% do calor corporal maximo
@@ -38,5 +42,31 @@ public class CharacterController : MonoBehaviour {
 
 	void UpdateHp(){
 		
+	}
+
+	void OnCollisionEnter(Collision collision){
+		//entra na zona
+		collision.collider.tag == "";
+		if (collision.gameObject.CompareTag("safe_zone_calor")) {
+			
+		}
+		else if(collision.gameObject.CompareTag("safe_zone_sanidade")){
+
+
+		}
+		
+	}
+
+	void OnCollisionExit(Collision collision){
+		//sai da zona
+
+		collision.collider.tag == "";
+		if (collision.gameObject.CompareTag("safe_zone_calor")) {
+
+		}
+		else if(collision.gameObject.CompareTag("safe_zone_sanidade")){
+
+
+		}
 	}
 }
