6ff5f44c0dfaaf80a96e2bb9c14fdeae64e21586
paulorobertocruz <paulo.cruz9@gmail.com>
2016-08-27 17:25:43 -0300

    adicionando script princiapal do palyer com esqueleto inicial de hp, sanidade e calor

---
+++ unity/Assets/scripts/CharacterController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	int hp;
+	public int max_hp = 0;
+
+	int sanidade;
+	public int max_sanidade = 0;
+
+	int calor_corporal;
+	public int max_calor_corporal = 0;
+
+	// Use this for initialization
+	void Start () {
+		hp = max_hp;
+
+		//
+		sanidade = max_sanidade / 2;
+
+		//começa com 10% do calor corporal maximo
+		calor_corporal = max_calor_corporal / 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void UpdateCalorCorporal(){
+		
+	}
+
+	void UpdateSanidade(){
+		
+	}
+
+	void UpdateHp(){
+		
+	}
+}
