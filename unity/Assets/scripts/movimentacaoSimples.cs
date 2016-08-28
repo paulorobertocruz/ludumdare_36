3efaca0e8ce68f42323e1f542f50ec404f9c66e0
Guz1992 <gustavogpi25@gmail.com>
2016-08-28 03:20:28 -0300

    tudo no jeito, indo dormir
    tem um script de teste de movimentação, eu estava olhando as cores do
    cenário, colocquei um material no chão mas é bom arrumarmos uma textura
    com normal.

---
+++ unity/Assets/scripts/movimentacaoSimples.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimentacaoSimples : MonoBehaviour {
+
+    float velocidade;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        velocidade = Time.deltaTime * 10;
+
+        //movimentos para frente/trás e lados
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, 0, velocidade));
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 0, -velocidade));
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up * 5);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up * -5);
+        }
+
+    }
+}
