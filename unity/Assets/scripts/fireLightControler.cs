8a45083e3a57633892c175e2e2d94c09766b6726
Guz1992 <gustavogpi25@gmail.com>
2016-08-28 02:48:23 -0300

    script fogo melhorado, iniciando prefab tocha

---
+++ unity/Assets/scripts/fireLightControler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireLightControler : MonoBehaviour {
+
+    public float duration = 0.1F;
+    public Light lt;
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+    void Update()
+    {
+        float phi = Time.time / duration * 2 * Mathf.PI;
+        float amplitude = Mathf.Cos(phi) * 0.9F + 0.5f;
+        lt.intensity = amplitude+0.5f;
+    }
+}
