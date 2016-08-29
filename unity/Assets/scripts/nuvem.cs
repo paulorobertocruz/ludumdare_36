3d9292fd44f7ff1cd61546594dce424adad5164f
Guz1992 <gustavogpi25@gmail.com>
2016-08-29 18:17:58 -0300

    fogo random no cenário adicionado, terminando sistema de contar tempo de vida do fogo principal

---
+++ unity/Assets/scripts/nuvem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nuvem : MonoBehaviour {
+
+    // Use this for initialization
+    float position;
+    float tempo;
+
+    public GameObject fogo;
+    
+    void Start () {
+	}
+
+    // Update is called once per frame
+    void Update () {
+        tempo += Time.deltaTime;
+
+        if (tempo >= 5) {
+            Instantiate(fogo,transform.position,transform.rotation);
+            tempo = 0;
+        }
+        transform.transform.position = new Vector3(Random.Range(86f, 410f), 9, Random.Range(90f, 390f));
+
+    }
+}
